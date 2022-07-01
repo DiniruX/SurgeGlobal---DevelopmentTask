df9de6548b0749383865e9dd172c66d8eefc6e2d
Diniru Sandipa <it20041120@my.sliit.lk>
2022-07-01 22:16:50 +0530

    note and user routes called in server.js

--- Backend/server.js
+++ Backend/server.js
@@ -7,6 +7,8 @@ const DB_URL = 'mongodb+srv://admin:admin@mynotes.zeocl.mongodb.net/MyNotes?retr
 const app = express();
 
 //import routes
+const noteRoutes = require('./Routes/NoteRoutes');
+const userRoutes = require('./Routes/UserRoutes');
 
 app.use(cors({
 
@@ -17,6 +19,8 @@ app.use(bodyParser.urlencoded({ extended: true}));
 app.use(bodyParser.json());
 
 //use routes
+app.use("/notes",noteRoutes);
+app.use("/users", userRoutes);
 
 mongoose.connect(DB_URL, {
 
